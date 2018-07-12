fc70e1882f659772e88f5124569e8928ffe35bfa
Francisco Hernandez <fhernandezj@uninorte.edu.co>
2018-07-12 10:40:53 -0500

    User components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { UserNewComponent } from './user/user-new/user-new.component';
+import { UserListComponent } from './user/user-list/user-list.component';
+import { UserEditComponent } from './user/user-edit/user-edit.component';
+import { UserDetailsComponent } from './user/user-details/user-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    UserNewComponent,
+    UserListComponent,
+    UserEditComponent,
+    UserDetailsComponent
   ],
   imports: [
     BrowserModule
